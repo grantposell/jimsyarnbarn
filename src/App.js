1284c6d5f16e7aedcf0ef3ed8c6e786b74d1b2a1
grantposell <grantposell@gmail.com>
2019-04-25 22:31:34 -0500

    Tweaking css to fix menu toggle

--- src/App.js
+++ src/App.js
@@ -8,42 +8,41 @@ import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover', height: '100vh'}}>
-        <Header transparent title="Jim's Yarn Barn" style={{color: 'white'}}>
+      <div style={{ height: '300px', position: 'relative' }}>
+        <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover', height: '100vh' }}>
+          <Header transparent title="Jim's Yarn Barn" style={{ color: 'white' }}>
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+              <a href="/">Home</a>
+              <a href="./store">Store</a>
+              <a href="./cart">Cart</a>
             </Navigation>
-        </Header>
-        <Drawer title="Title">
+          </Header>
+          <Drawer title="Jim's Yarn Barn">
             <Navigation>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
-                <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
             </Navigation>
-        </Drawer>
-        <Content />
-        <Content>
+          </Drawer>
+          <Content />
+          <Content>
             <div className="page-content" />
             <Main></Main>
           </Content>
-        <Footer size="mini">
+          <Footer size="mini">
             <FooterSection type="left" logo="Jim's Yarn Barn © 2019" >
               <FooterLinkList>
                 <a href="/privacy">Privacy & Legal</a>
                 <a href="/contact">Contact</a>
                 <a href="/careers">Careers</a>
                 <a href="#">Get Newsletter</a>
-                <a href="/forums">Forums</a>
+                <a href="/forums">Blog</a>
               </FooterLinkList>
             </FooterSection>
           </Footer>
-    </Layout>
-</div>
+        </Layout>
+      </div>
     );
   }
 }
